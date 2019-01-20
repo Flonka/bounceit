6f44281b926b7c8f119757fd46c0dcb5f5e7af6b
Flonka <flonkation@gmail.com>
2019-01-20 23:21:56 +0200

    Use kinematicbody properly for movement.

--- scenes/paddle/Paddle.gd
+++ scenes/paddle/Paddle.gd
@@ -4,27 +4,24 @@ extends KinematicBody2D
 enum AxisUpdate {X,Y}
 export(AxisUpdate) var axis_lock = AxisUpdate.X
 
+var axis_vec : Vector2
+
+const SPEED : float = 10.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	print(axis_lock)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-func _physics_process(delta: float) -> void:
-	get_input()
-
-func get_input() -> void:
-	var mouse_pos: Vector2 = get_global_mouse_position()
 	match axis_lock:
 		AxisUpdate.X:
-			position.x = mouse_pos.x
+			axis_vec = Vector2(1,0)
 		AxisUpdate.Y:
-			position.y = mouse_pos.y
+			axis_vec = Vector2(0,1)
+	axis_vec *= SPEED
+
+
+func _physics_process(delta :float) -> void:
+	var dir : Vector2 = get_global_mouse_position() - get_global_position()
+	move_and_slide(dir * axis_vec)
 
 
 
