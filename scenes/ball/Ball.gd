23998721aded0ee7fd642e6d8ecf1d4dd6403481
Flonka <flonkation@gmail.com>
2019-02-13 19:57:06 +0200

    Add note on trjaectory solution.

--- scenes/ball/Ball.gd
+++ scenes/ball/Ball.gd
@@ -12,6 +12,7 @@ func _ready() -> void:
 	_velocity = Vector2(randf(), randf()).normalized() * init_velocity
 
 func _physics_process(delta: float) -> void:
+	# https://stackoverflow.com/questions/41210110/curved-trajectory-for-a-2d-game-in-java
 	var ci : = move_and_collide(_velocity)
 	if ci:
 		_velocity = _velocity.bounce(ci.normal)
