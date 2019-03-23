ee1e33c4987f3fae6bc2c405f5086ea30d260ef2
Flonka <flonkation@gmail.com>
2019-03-23 13:34:23 +0200

    Working position update with mode switching.

--- scenes/rbpaddle/RbKinematidPaddle.gd
+++ scenes/rbpaddle/RbKinematidPaddle.gd
@@ -7,6 +7,9 @@ export(AxisUpdate) var axis_lock = AxisUpdate.XY
 
 var _axis_vector: Vector2
 
+onready var timer : Timer = $RigidTimer
+onready var tween : Tween = $Tween
+
 func _ready() -> void:
 	match axis_lock:
 		AxisUpdate.X:
@@ -16,14 +19,28 @@ func _ready() -> void:
 		AxisUpdate.XY:
 			_axis_vector = Vector2(1,1)
 
+	timer.set_one_shot(true)
+	timer.set_wait_time(1.5)
+
 #warning-ignore:unused_argument
 func _physics_process(delta: float) -> void:
-	if mode == RigidBody2D.MODE_KINEMATIC:
+	if mode == RigidBody2D.MODE_KINEMATIC and not tween.is_active():
 		set_global_position(get_global_mouse_position() * _axis_vector)
+
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	var xform : Transform2D = state.get_transform()
+	xform.origin = get_global_position()
+	state.set_transform(xform)
 
 func ball_hit(collision_object: KinematicCollision2D, ball_velocity : Vector2) -> void:
 	if self.mode != RigidBody2D.MODE_RIGID:
-		self.mode = RigidBody2D.MODE_RIGID
-		# Bug https://github.com/godotengine/godot/issues/25738
-		self.sleeping = false
-		self.apply_impulse(get_global_position() - collision_object.position, ball_velocity)
+		self.set_mode(RigidBody2D.MODE_RIGID)
+
+		if timer.is_stopped():
+			timer.start()
+
+func _on_RigidTimer_timeout() -> void:
+	self.set_mode(RigidBody2D.MODE_KINEMATIC)
+
+
+
