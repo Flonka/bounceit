81aab8f2b9ed24649c48b02744429f740969c395
Flonka <flonkation@gmail.com>
2019-03-24 20:08:46 +0200

    freeze bug fix, need two tweens to differ between position and rotation tweens active states.

--- scenes/rbpaddle/RbKinematidPaddle.gd
+++ scenes/rbpaddle/RbKinematidPaddle.gd
@@ -6,12 +6,13 @@ enum AxisUpdate {X,Y,XY}
 export(AxisUpdate) var axis_lock = AxisUpdate.XY
 
 const position_tween_duration := 1.1
-const rotation_tween_duration := 1.5
+const rotation_tween_duration := 2
 
 var _axis_vector: Vector2
 
 onready var timer : Timer = $RigidTimer
-onready var tween : Tween = $Tween
+onready var position_tween := $PositionTween
+onready var rotation_tween := $RotationTween
 onready var original_rotation := self.get_rotation()
 
 func _ready() -> void:
@@ -28,14 +29,15 @@ func _ready() -> void:
 
 #warning-ignore:unused_argument
 func _physics_process(delta: float) -> void:
-	if mode == RigidBody2D.MODE_KINEMATIC and not tween.is_active():
+	if mode == RigidBody2D.MODE_KINEMATIC and not position_tween.is_active():
 		set_global_position(get_global_mouse_position() * _axis_vector)
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 
 	if self.mode == RigidBody2D.MODE_KINEMATIC:
-		var xform : Transform2D = state.get_transform()
+		var xform : Transform2D = state.get_transform().orthonormalized()
 		xform.origin = get_global_position()
+		xform.rotated(self.get_rotation())
 		state.set_transform(xform)
 
 func ball_hit(collision_object: KinematicCollision2D, ball_velocity : Vector2) -> void:
@@ -49,15 +51,19 @@ func ball_hit(collision_object: KinematicCollision2D, ball_velocity : Vector2) -
 
 func _on_RigidTimer_timeout() -> void:
 	self.set_mode(RigidBody2D.MODE_KINEMATIC)
-	tween.follow_method(
+	self._add_tweening()
+
+func _add_tweening() -> void:
+	position_tween.follow_method(
 		self, "set_global_position", self.get_global_position(),
 		self, "get_global_mouse_position", self.position_tween_duration,
 		Tween.TRANS_ELASTIC, Tween.EASE_OUT)
-	tween.interpolate_property(
+	rotation_tween.interpolate_property(
 		self, "rotation", self.get_rotation(),
 		self.original_rotation, self.rotation_tween_duration,
 		Tween.TRANS_ELASTIC, Tween.EASE_OUT)
-	tween.start()
+	position_tween.start()
+	rotation_tween.start()
 
-
-
+func _on_Tween_tween_completed(object: Object, key: NodePath) -> void:
+	print_debug("tween "+ str(object)  + key + " complete")
