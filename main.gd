7573cb70819af7825a805f0e13bcefbcf96b65b0
Flonka <flonkation@gmail.com>
2019-03-25 20:48:26 +0200

    wip , build main scene.

--- main.gd
+++ main.gd
@@ -1,10 +1,32 @@
 extends Node
 
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+	var s := get_viewport().get_size()
+	print(str(s))
+
+	var paddle_scene := preload("res://scenes/rbpaddle/RbKinematidPaddle.tscn")
+
+	var padding := 200
+
+	var top_paddle : Paddle = paddle_scene.instance()
+	top_paddle.set_axis_lock(Paddle.AxisUpdate.X)
+	top_paddle.set_global_position(Vector2(0, padding))
+	#self.add_child(top_paddle)
+
+	var bottom_paddle := paddle_scene.instance()
+	bottom_paddle.set_axis_lock(Paddle.AxisUpdate.X)
+	bottom_paddle.set_position(Vector2(0, s.y - padding))
+	#self.add_child(bottom_paddle)
+
+	var right_paddle := paddle_scene.instance()
+	right_paddle.set_axis_lock(Paddle.AxisUpdate.Y)
+	right_paddle.set_position(Vector2(s.x - padding, 0))
+	#self.add_child(right_paddle)
+
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_ESCAPE:
 			get_tree().quit()
-
-func _ready() -> void:
-	randomize()
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
--- scenes/rbpaddle/RbKinematidPaddle.gd
+++ scenes/rbpaddle/RbKinematidPaddle.gd
@@ -3,20 +3,26 @@ extends RigidBody2D
 class_name Paddle
 
 enum AxisUpdate {X,Y,XY}
-export(AxisUpdate) var axis_lock = AxisUpdate.XY
-
-const position_tween_duration := 1.1
-const rotation_tween_duration := 2
-
-var _axis_vector: Vector2
 
 onready var timer : Timer = $RigidTimer
 onready var position_tween := $PositionTween
 onready var rotation_tween := $RotationTween
-onready var original_rotation := self.get_rotation()
+
+const position_tween_duration := 1.1
+const rotation_tween_duration := 2.0
+const time_in_rigid_mode := 1.5
+
+var original_rotation := self.get_rotation()
+
+var _axis_vector: Vector2
 
 func _ready() -> void:
-	match axis_lock:
+	print_debug("Paddle ready called")
+	timer.set_one_shot(true)
+	timer.set_wait_time(time_in_rigid_mode)
+
+func set_axis_lock(lock : int) -> void:
+	match lock:
 		AxisUpdate.X:
 			_axis_vector = Vector2(1,0)
 		AxisUpdate.Y:
@@ -24,16 +30,13 @@ func _ready() -> void:
 		AxisUpdate.XY:
 			_axis_vector = Vector2(1,1)
 
-	timer.set_one_shot(true)
-	timer.set_wait_time(0.7)
-
 #warning-ignore:unused_argument
 func _physics_process(delta: float) -> void:
-	if mode == RigidBody2D.MODE_KINEMATIC and not position_tween.is_active():
-		set_global_position(get_global_mouse_position() * _axis_vector)
+	if mode == RigidBody2D.MODE_KINEMATIC:
+		if not position_tween.is_active():
+			set_global_position(get_global_mouse_position() * _axis_vector)
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
-
 	if self.mode == RigidBody2D.MODE_KINEMATIC:
 		var xform : Transform2D = state.get_transform().orthonormalized()
 		xform.origin = get_global_position()
@@ -41,10 +44,9 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 		state.set_transform(xform)
 
 func ball_hit(collision_object: KinematicCollision2D, ball_velocity : Vector2) -> void:
-	if self.mode != RigidBody2D.MODE_RIGID:
-		print("set to rigid")
-		self.set_mode(RigidBody2D.MODE_RIGID)
 
+	if self.mode != RigidBody2D.MODE_RIGID:
+		self.set_mode(RigidBody2D.MODE_RIGID)
 		if timer.is_stopped():
 			print_debug("start timer")
 			timer.start()
@@ -58,10 +60,12 @@ func _add_tweening() -> void:
 		self, "set_global_position", self.get_global_position(),
 		self, "get_global_mouse_position", self.position_tween_duration,
 		Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+
 	rotation_tween.interpolate_property(
 		self, "rotation", self.get_rotation(),
 		self.original_rotation, self.rotation_tween_duration,
 		Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+
 	position_tween.start()
 	rotation_tween.start()
 
